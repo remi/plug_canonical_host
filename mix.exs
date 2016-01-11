19b8e27bd5920b73528912166e5e21b10fc2bdf0
Rémi Prévost <remi@exomel.com>
2016-01-11 17:03:38 -0500

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugCanonicalHost.Mixfile do
 
   def project do
     [app: :plug_canonical_host,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.0",
      deps: deps,
      package: package,
