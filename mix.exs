2188e54fa3049e405491eedba8ad1961e4f7098b
Rémi Prévost <remi@exomel.com>
2016-01-12 06:06:54 -0500

    Use ex_doc in dev

--- mix.exs
+++ mix.exs
@@ -23,8 +23,8 @@ defmodule PlugCanonicalHost.Mixfile do
   defp deps do
     [
       {:plug, " ~> 1.0"},
-      {:earmark, "~> 0.1", only: :docs},
-      {:ex_doc, "~> 0.11", only: :docs}
+      {:earmark, "~> 0.1", only: :dev},
+      {:ex_doc, "~> 0.11", only: :dev}
     ]
   end
 
