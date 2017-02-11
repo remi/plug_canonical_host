6a150b1221c852eaade878c2ced1ca91764e0d77
Rémi Prévost <remi@exomel.com>
2017-02-11 11:42:54 -0500

    Improve test code clarity

--- test/plug_canonical_host_test.exs
+++ test/plug_canonical_host_test.exs
@@ -14,22 +14,20 @@ defmodule PlugCanonicalHostTest do
     end
   end
 
-  @opts TestApp.init([])
-
   defp get(uri) do
     conn(:get, uri)
     |> TestApp.call(TestApp.init([]))
   end
 
   test "redirects to canonical host" do
-    conn = get(URI.parse("http://www.example.com/foo?bar=1"))
+    conn = get("http://www.example.com/foo?bar=1")
 
     assert conn.status == 301
     assert get_resp_header(conn, "location") === ["http://example.com/foo?bar=1"]
   end
 
   test "does not redirect to canonical host when already on canonical host" do
-    conn = get(URI.parse("http://example.com/foo"))
+    conn = get("http://example.com/foo")
 
     assert conn.status == 200
     assert conn.resp_body == "Hello World"
